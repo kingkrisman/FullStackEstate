4a29ee7cc688845011e8cde47f696c24798bc5d4
kingkrisman <kingkrisman@gmail.com>
2024-11-08 09:49:10 -1100

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
   return<BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/signin" element={<Signin />} />
